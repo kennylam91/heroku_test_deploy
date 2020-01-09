1f6fdfb7287f3186e2e0c69dd3a3f7e6ffe80868
Pham Van Lam <phamvanlam1991@gmail.com>
2020-01-09 14:45:53 +0700

    first cm

---
+++ src/main/java/com/newsapp/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.newsapp.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public ResponseEntity<String> hello(@RequestParam(defaultValue = "") String name){
+		return new ResponseEntity<>("Hello " + name,HttpStatus.OK);
+	}
+}
